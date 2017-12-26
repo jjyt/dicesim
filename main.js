8e5abcc5fbcf076e5893ec0e39db6f4da9f665e9
Oruaro Emiaso <oruaroemiaso@gmail.com>
2017-12-26 20:51:27 +0000

    Update

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 window.onload = function () {
-    var balance = document.getElementById('balance');
-    var maxlosses = document.getElementById('maxlosses');
-    var baseBet = document.getElementById('basebet');
-    var betincrease = document.getElementById('betincrease');
-    var betchance = document.getElementById('betchance');
-    var wincondition = document.getElementById('wincondition');
-    var betchance = document.getElementById('bank');
-    var simIsRunning = false;
+    var balance = document.getElementById('balance').value;//
+    var maxlosses = document.getElementById('maxlosses').value;//
+    var maxlossesvalue = document.getElementById('maxlossesvalue').value;//
+    var baseBet = document.getElementById('basebet').value;//
+    var betincrease = document.getElementById('betincrease').value;//
+    var betbatches = document.getElementById('betbatches').value;//
+    var betchance = document.getElementById('betchance').value;//
+    var wincondition = document.getElementById('wincondition').value;//
+    var bank = document.getElementById('bank'.value);//
+
+    function getRandomIntInclusive(min, max) {
+        const randomBuffer = new Uint32Array(1);
+    
+        window.crypto.getRandomValues(randomBuffer);
+    
+        let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+    
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(randomNumber * (max - min + 1)) + min;
+    }
 
     function formatInputBTC(event) {
         this.value = parseFloat(this.value).toFixed(8);
@@ -19,34 +32,35 @@ window.onload = function () {
         this.value = parseFloat(this.value).toFixed(2);
     }
 
+    function formatInputIncrease(event) {
+        if(this.value <= 1 || this.value >= 20.00){
+            this.value = 2.00;
+        }
+        this.value = parseFloat(this.value).toFixed(2);
+    }
+    
     function startSim() {
         if (document.getElementById('simbutton').innerText === "Start Sim" ){
             document.getElementById('simbutton').innerText = "Stop Sim";
-            simIsRunning = !simIsRunning;
-
+            var rolls = []
+            for (var i = 1; i <= this.betbatches; i++) {
+                let roll = getRandomIntInclusive(1,9999);
+                alert(roll);
+                rolls.push(roll);
+            }
+            alert(rolls.length);
         } 
         else {
             document.getElementById('simbutton').innerText = "Start Sim";
-            simIsRunning = !simIsRunning;
         }
     }
 
-    document.getElementById('simbutton').addEventListener('click', function(){startSim()});
-    
+    // * Event Listeners * //
+    document.getElementById('simbutton').addEventListener('click', startSim);
     document.getElementById('balance').addEventListener('change', formatInputBTC);
     document.getElementById('bank').addEventListener('change', formatInputBTC);
     document.getElementById('basebet').addEventListener('change', formatInputBTC);
     document.getElementById('betchance').addEventListener('change', formatInputChance);
-    
+    document.getElementById('wincondition').addEventListener('change', formatInputBTC);
+    document.getElementById('maxlossesvalue').addEventListener('change', formatInputBTC);
 };
-//startingBalance = float64(600) // int64 so can be big if you're using DOGE for safer betting or smth
-//maxBet = float64(43.14320869)
-//baseBet = float64(0.00015000)
-//currentBet = baseBet
-//betIncrease := float64(1.5)
-//betChance = int64(2488) // ( 0 - 9999 ) 5000 would be ~50%
-
-//winCondition = 601
-//bank = 0
-//losses := 0
-//lossMaxBets := 0
